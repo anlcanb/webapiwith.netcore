c38cb4e11aebdf3cea8408762cdfde4d3fdae63b
anlcanb <bostancianil84@gmail.com>
2025-08-18 14:06:40 +0300

    ilk commit

---
+++ Context/ApiContext.cs
@@ -0,0 +1,45 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class ApiContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    // Parametresiz constructor ekledik → migration sırasında çalışsın diye
+    public ApiContext()
+    {
+        // Migration esnasında appsettings.json'dan bağlantı alacak
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        _configuration = builder.Build();
+    }
+
+    public ApiContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Chef> Chefs { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<Image> Images { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+}
